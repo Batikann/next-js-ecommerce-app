edcde0432e2d93bc2e0924099392854ccffb4d71
Batikann <emiruar123@gmail.com>
2024-04-25 21:36:31 +0300

    admin dashboard page

---
+++ components/DashboardCard.tsx
@@ -0,0 +1,23 @@
+import { DashboardCardType } from '@/lib/types'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+
+const DashboardCard = ({ title, subtitle, body }: DashboardCardType) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  )
+}
+export default DashboardCard
---
+++ lib/types.ts
@@ -0,0 +1,5 @@
+export type DashboardCardType = {
+  title: string
+  subtitle: string
+  body: string
+}
